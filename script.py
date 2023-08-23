f3b642c11dc528594a923e07dd1b0a930971350c
gmap-nitro <geraldo.filho92@hotmail.com>
2023-08-23 19:08:03 -0300

    :tada: add Web version

---
+++ script.py
@@ -0,0 +1,35 @@
+from docx import Document
+from docx.shared import Pt
+
+
+
+documento = Document("ETIQUETA.docx")
+
+nf = input('Nota fiscal:')
+vendacasada = input('Num venda casada:')
+origem = input('Num origem:')
+destino = input('Num destino:')
+
+
+
+# Dictionaries
+referencias = {
+   "XXXX": nf,
+   "YYYYY": vendacasada,
+   "ZZZZ": origem,
+   "kkkk": destino,
+}
+
+
+for paragrafo in documento.paragraphs:
+   inline = paragrafo.runs
+   for i in range(len(inline)):
+      text = inline[i].text      
+      for codigo in referencias.keys():
+         if codigo in text:
+            text=text.replace(codigo, referencias[codigo])
+            inline[i].text = text
+      
+
+documento.save("etiqueta lançada.docx")
+
